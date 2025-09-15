a190852ea9df1d5be4189b0b181b40f1b231fca0
foxmanuu <manoela.braga@escola.pr.gov.br>
2025-09-15 08:59:38 -0300

    Add files via upload

---
+++ principal.js
@@ -0,0 +1,43 @@
+var subtitulo = document.querySelector(".subtitulo");
+			subtitulo.textContent = "Meus Pacientes";
+			var logo = document.querySelector(".titulo");
+			logo.textContent = "Svt nutricion";
+
+	var pacientes = document.querySelectorAll(".paciente");
+
+	for(var i = 0; i < pacientes.length; i++){
+    var paciente = pacientes[i];
+
+    //seleciona o conteúdo peso da tag
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+	console.log(peso);
+
+    // seleciona o conteúdo altura da tag
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+	console.log(altura);
+
+    //calcula o imc
+    var imc = peso / (altura * altura);
+	console.log(imc);
+
+    //variáveis com valor true
+    var pesoValido = true;
+    var alturaValida = true; 
+
+    if(pesoValido && alturaValida){
+        // acessa e altera o imc
+        var tdImc = paciente.querySelector(".info-imc");
+        tdImc.textContent = imc;
+    }
+    //define limites de peso e altura
+    if(peso <= 0 || peso >= 1000){
+        var pesoValido = false;
+        tdImc.textContent = "Peso Inválido!";
+    }
+    if(altura <= 0 || altura >= 3.00){
+        var alturaValida = false;
+        tdImc.textContent = "Altura Inválida";
+    }
+	}
